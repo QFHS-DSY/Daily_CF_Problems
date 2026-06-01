7e6485b551fbe9fb911be06f16b8dc69b6930897
杜松煜 <dsy20070704@163.com>
2026-06-01 14:24:53 +0800

    260601

--- I_KSumT.cpp
+++ I_KSumT.cpp
@@ -11,8 +11,18 @@
 using namespace std;
 typedef long long ll;
 
-inline void solve()
-{
+constexpr ll MOD=(ll)1e9+7,M=(ll)5e6+5;
+
+inline ll ksm(ll b,ll e) {
+    b%=MOD;
+    ll ans=1;
+    while (e) {
+        if (e&1) (ans*=b)%=MOD;
+        e>>=1;
+        b=b*b%MOD;
+    }
+
+    return ans;
 }
 
 signed main()
@@ -21,10 +31,31 @@ signed main()
     cin.tie(nullptr), cout.tie(nullptr);
     //cout<<fixed<<setprecision(15);
 
-    int T;
-    cin>>T;
-    while(T--)
-        solve();
+	vector<int> f(M), g(M);
 
+	f[0]=1;
+	for (int i=1;i<M;i++) f[i]=1ll*i*f[i-1]%MOD;
+	g[M-1]=ksm(f[M-1],MOD-2);
+	for (int i=M-1;i>0;i--) g[i-1]=1ll*i*g[i]%MOD;
+
+	auto C=[&] (int x,int y) -> int {
+		if (x==y) return 1;
+		if (y<0 || x<0 || y>x) return 0;
+		return 1ll*f[x]*g[y]%MOD*g[x-y]%MOD;
+	};
+
+    int k,s,t;cin>>k>>s>>t;
+    int m=k/t,n=k%t;
+    
+    ll ans=0;
+    for (int i=1;i<=s;++i) {
+        if (i*m>s) break;
+        if ((s-i*m)%(m+1)==0) {
+            int c1=i,c2=(s-i*m)/(m+1);
+            (ans+=1ll*C(c1-1,t-n-1)*C(c2-1,n-1)%MOD)%=MOD;
+        }
+    }
+
+    cout<<ans<<endl;
     return 0;
 }
