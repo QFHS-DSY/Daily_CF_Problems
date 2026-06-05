34dd4187979b0ed17a701541a24994489a887f12
杜松煜 <17107841+du-songyu@user.noreply.gitee.com>
2026-06-05 13:42:26 +0800

    260605

--- B_Triangles_and_a_Circle.cpp
+++ B_Triangles_and_a_Circle.cpp
@@ -8,7 +8,7 @@
 #define PII pair<int, int>
 #define INF 0x3f3f3f3f
 #define LLINF 0x3f3f3f3f3f3f3f3fLL
-// #define int long long
+#define int long long
 using namespace std;
 using ll=long long;
 using ld=long double;
@@ -20,7 +20,17 @@ signed main()
     //cout<<fixed<<setprecision(15);
 
     int n,l;cin>>n>>l;
-    
+    vector<int> a(2*n+1);
+    for (int i=1;i<=n;++i) cin>>a[i];
+    sort(a.begin()+1,a.begin()+n+1);
+    for (int i=1;i<=n;++i) a[i+n]=a[i]+l;
 
+    ll ans=n*(n-1)/2ll*(n-2)/3ll,r=1;
+    for (int i=1;i<=n;++i) {
+        while ((a[r]-a[i])*2<l) r++;
+        ans-=1ll*(r-i-1)*(r-i-2)/2;
+        //cout<<i<<" "<<r<<endl;
+    }
+    cout<<ans<<endl;
     return 0;
 }
