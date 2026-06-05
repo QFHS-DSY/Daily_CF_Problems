1e2d58b4d3967859cfe6401b111c12aa2f639717
杜松煜 <dsy20070704@163.com>
2026-06-05 10:30:49 +0800

    260605

---
+++ B_Triangles_and_a_Circle.cpp
@@ -0,0 +1,26 @@
+//https://codeforces.com/gym/102860/problem/B
+//260605
+//1400
+#include <bits/stdc++.h>
+#define endl '\n'
+#define fi first
+#define se second
+#define PII pair<int, int>
+#define INF 0x3f3f3f3f
+#define LLINF 0x3f3f3f3f3f3f3f3fLL
+// #define int long long
+using namespace std;
+using ll=long long;
+using ld=long double;
+
+signed main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr), cout.tie(nullptr);
+    //cout<<fixed<<setprecision(15);
+
+    int n,l;cin>>n>>l;
+    
+
+    return 0;
+}
