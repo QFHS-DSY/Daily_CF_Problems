6a915e77bcf0777756b7c4c1a0d16580f5a07d73
杜松煜 <dsy20070704@163.com>
2026-06-12 08:54:59 +0800

    260612

--- F_Pinholes.cpp
+++ F_Pinholes.cpp
@@ -8,7 +8,7 @@
 #define PII pair<int, int>
 #define INF 0x3f3f3f3f
 #define LLINF 0x3f3f3f3f3f3f3f3fLL
-// #define int long long
+#define int long long
 using namespace std;
 using ll=long long;
 using ld=long double;
@@ -19,10 +19,82 @@ signed main()
     cin.tie(nullptr), cout.tie(nullptr);
     //cout<<fixed<<setprecision(15);
 
-    int n,m;cin>>n>>m;
-    vector<int> a(n),b(m);
-    for (auto &x : a) cin>>x;
-    for (auto &x : b) cin>>x;
+    // int n,m;cin>>n>>m;
+    // vector<int> a(n),b(m);
+    // set<int> ans;
+    // for (auto &x : a) cin>>x;
+    // for (auto &x : b) cin>>x;
+
+    // sort(a.begin(),a.end());
+    // sort(b.begin(),b.end());
+
+    // for (int x : a) 
+    //     for (int y: b) 
+    //         ans.insert(2*x-y);
+
+    // map<int,int> mp;
+    // int cn=ans.size();
+    // for (int x : ans) {
+    //     mp[x]=1;
+    //     for (int y : a) {
+    //         if (find(b.begin(),b.end(),2*y-x)==b.end()) {
+    //             mp[x]=0;
+    //             cn--;
+    //             break;
+    //         }
+    //     }
+    // }
+
+    // if (cn==0) cout<<"No"<<endl;
+    // else {
+    //     cout<<"Yes"<<endl;
+    //     cout<<cn<<endl;
+    //     for (int x : ans) if (mp[x]) cout<<x<<" ";
+    //     cout<<endl;
+    // }
+
+
+	int n, m;
+	cin >> n >> m;
+
+	vector<int> holes(n), targets(m);
+
+	for (auto &x: holes) cin >> x;
+	for (auto &x: targets) cin >> x;
+
+	sort(holes.begin(), holes.end());
+	sort(targets.begin(), targets.end());
+
+	vector<int> ans, vis(m, 0);
+
+	for (auto &v: targets) {
+		int pt = 0;
+		bool flg = true;
+
+		for (auto &x: holes) {
+			while (pt < m && targets[pt] < v + 2 * (x - holes[0])) pt ++;
+			if (pt == m || targets[pt] != v + 2 * (x - holes[0])) flg = false;
+		}
+
+		if (flg) {
+			ans.emplace_back(2 * holes[0] - v);
+			pt = 0;
+
+			for (auto &x: holes) {
+				while (pt < m && targets[pt] < v + 2 * (x - holes[0])) pt ++;
+				vis[pt] = 1;
+			}
+		}
+	}
+
+	if (*min_element(vis.begin(), vis.end())) {
+		cout << "Yes\n";
+		cout << ans.size() << '\n';
+		for (int i = 0; i < ans.size(); i ++)
+			cout << ans[i] << " \n"[i + 1 == ans.size()];
+	}
+	else cout << "No\n";
+
 
     return 0;
 }
--- H_Heading_to_Graduation.cpp
+++ H_Heading_to_Graduation.cpp
@@ -2,7 +2,7 @@
 //260519
 //1700
 #include <bits/stdc++.h>
-#define endl '\n'
+#define endl "\n"
 #define fi first
 #define se second
 #define PII pair<int, int>
@@ -12,20 +12,47 @@
 using namespace std;
 typedef long long ll;
 
-inline void solve()
-{
-}
+constexpr int MAXN=200005;
+int n,m,k,deg[MAXN],cnt=0,ans;
+vector<int> g[MAXN];
+set<int> f[MAXN];
 
 signed main()
 {
     ios::sync_with_stdio(false);
     cin.tie(nullptr), cout.tie(nullptr);
-    //cout<<fixed<<setprecision(15);
 
-    int T;
-    cin>>T;
-    while(T--)
-        solve();
+    cin>>n>>m;
+    for (int j=1,x,y;j<=m;++j) {
+        cin>>x>>y;
+        deg[y]++;
+        g[x].push_back(y);
+    }
+    cin>>k;
+    for (int i=1,x,j;i<=k;++i) {
+        cin>>j>>x;
+        f[x].insert(j);
+    }
 
+    queue<int> q,cq;
+    for (int i=1;i<=n;++i) 
+        if (!deg[i]) q.push(i);
+    
+    ans=1;
+    while (!q.empty() || !cq.empty()) {
+        if (q.empty()) {q=cq;while(!cq.empty()) cq.pop(); ans++;}
+        int x=q.front();q.pop();
+        if (f[x].find(ans)!=f[x].end()) cq.push(x);
+        else {
+            //cout<<x<<" "<<ans<<endl;
+            cnt++;
+            for (int y : g[x]) {
+                deg[y]--;
+                if (!deg[y]) cq.push(y);
+            }
+        }
+    }
+    if (cnt!=n) cout<<-1<<endl;
+    else cout<<ans<<endl;
     return 0;
 }
