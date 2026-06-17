d290b72e4c603f5b6146e6139feb9a5c5e66df8e
杜松煜 <dsy20070704@163.com>
2026-06-17 12:00:22 +0800

    260617

---
+++ C_Renan_and_Cirque_du_Soleil.cpp
@@ -0,0 +1,32 @@
+//https://codeforces.com/gym/101962/problem/C
+//260617
+//1400
+#include <bits/stdc++.h>
+#define endl '\n'
+#define fi first
+#define se second
+#define PII pair<int, int>
+#define INF 0x3f3f3f3f
+#define LLINF 0x3f3f3f3f3f3f3f3fLL
+// #define int long long
+using namespace std;
+using ll=long long;
+using ld=long double;
+
+inline void solve()
+{
+}
+
+signed main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr), cout.tie(nullptr);
+    //cout<<fixed<<setprecision(15);
+
+    int T;
+    cin>>T;
+    while(T--)
+        solve();
+
+    return 0;
+}
--- K_Fellow_Sheep.cpp
+++ K_Fellow_Sheep.cpp
@@ -13,20 +13,40 @@ using namespace std;
 using ll=long long;
 using ld=long double;
 
-inline void solve()
-{
-}
-
 signed main()
 {
     ios::sync_with_stdio(false);
     cin.tie(nullptr), cout.tie(nullptr);
     //cout<<fixed<<setprecision(15);
 
-    int T;
-    cin>>T;
-    while(T--)
-        solve();
+    // a   b
+    //   c  
+    // d   e
 
+    int n;cin>>n;
+    ll t=LLONG_MAX;
+    for (int i=1,a,b,c,d,e;i<=n;++i) {
+        cin>>a>>b>>c>>d>>e;
+        ll ans=0;
+        if (b>=a) {
+            ans+=a;
+            if (e>=d) ans+=d;
+            else {
+                ans+=e;
+                int tmp=min(c,b-a);
+                ans+=min(tmp,d-e);
+            }
+        } else {
+            ans+=b;
+            if (e>=d) {
+                ans+=d;
+                int tmp=min(c,e-d);
+                ans+=min(tmp,a-b);
+            } else ans+=e;
+        }
+        t=min(t,ans);
+    }
+
+    cout<<t<<endl;
     return 0;
 }
