ec556ea4d2012185960fec2cd1e9d14e7d661903
杜松煜 <dsy20070704@163.com>
2026-06-17 16:55:53 +0800

    260617

--- C_Renan_and_Cirque_du_Soleil.cpp
+++ C_Renan_and_Cirque_du_Soleil.cpp
@@ -13,8 +13,30 @@ using namespace std;
 using ll=long long;
 using ld=long double;
 
+constexpr ll MOD=(ll)1e9+7;
+
+ll ksm(ll b,ll e) {
+    b%=MOD;
+    ll res=1;
+    while (e) {
+        if (e&1) (res*=b)%=MOD;
+        b=b*b%MOD;
+        e>>=1;
+    }
+    return res;
+}
+
 inline void solve()
 {
+    ll n;cin>>n;
+    if (n==1) cout<<0<<endl;
+    else if (n==2) cout<<1<<endl;
+    else {
+        ll ans=ksm(2ll,n-1);
+        ans=ans*((2*n-6)%MOD)%MOD;
+        (ans+=n+3)%=MOD;
+        cout<<ans<<endl;
+    }
 }
 
 signed main()
