71e2525df0f69285cbe70cab615e888753fe2446
杜松煜 <dsy20070704@163.com>
2026-06-27 23:48:30 +0800

    260627

--- B_Mukhtar_s_Escape_Plan.cpp
+++ B_Mukhtar_s_Escape_Plan.cpp
@@ -13,20 +13,57 @@ using namespace std;
 using ll=long long;
 using ld=long double;
 
-inline void solve()
-{
-}
-
 signed main()
 {
     ios::sync_with_stdio(false);
     cin.tie(nullptr), cout.tie(nullptr);
     //cout<<fixed<<setprecision(15);
 
-    int T;
-    cin>>T;
-    while(T--)
-        solve();
+    int n,m,k;
+    ll c;
+    cin>>n>>m>>k>>c;
+    vector<bool> is(n+1,0);
+    vector<ll> f(n+1);
+    vector<vector<int>> g(n+1);
+    for (int i=1,x;i<=k;++i) cin>>x,is[x]=1;
+    for (int i=1;i<=n;++i) cin>>f[i];
+    for (int i=0,u,v;i<m;++i) {
+        cin>>u>>v;
+        g[u].push_back(v);
+        g[v].push_back(u);
+    }
+
+    vector<ll> dis(n+1,LLINF);
+    priority_queue<pair<ll,int>,vector<pair<ll,int>>,greater<pair<ll,int>>> q;
+    dis[1]=0;
+    q.push({0,1});
+
+    while (!q.empty()) {
+        auto [d,u]=q.top();
+        q.pop();
+        if (d!=dis[u]) continue;
+        if (u==n) break;
+
+        if (is[u]) {
+            for (auto v:g[u]) {
+                if (is[v]) continue;
+                if (d<dis[v]) {
+                    dis[v]=d;
+                    q.push({dis[v],v});
+                }
+            }
+        } else {
+            for (auto v:g[u]) {
+                ll w=is[v]?f[v]:0;
+                if (d+w<dis[v]) {
+                    dis[v]=d+w;
+                    q.push({dis[v],v});
+                }
+            }
+        }
+    }
+
+    cout<<(dis[n]<=c?dis[n]:-1)<<endl;
 
     return 0;
 }
