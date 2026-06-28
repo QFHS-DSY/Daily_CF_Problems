ecd9b83b6544365e233dbc0b0638947d9023c244
杜松煜 <dsy20070704@163.com>
2026-06-28 21:39:33 +0800

    260629

--- A_Tree_Labeling.cpp
+++ A_Tree_Labeling.cpp
@@ -15,6 +15,62 @@ using ld=long double;
 
 inline void solve()
 {
+    int n;
+    cin>>n;
+
+    vector<vector<int>> g(n+1);
+    for(int i=1;i<n;i++)
+    {
+        int u,v;
+        cin>>u>>v;
+        g[u].push_back(v);
+        g[v].push_back(u);
+    }
+
+    vector<int> color(n+1,-1), mask(n+1,7);
+    auto assign_color=[&](int st,int c)
+    {
+        vector<int> stk;
+        mask[st]=1<<c;
+        stk.push_back(st);
+        while(!stk.empty())
+        {
+            int u=stk.back();
+            stk.pop_back();
+
+            if(color[u]!=-1)
+                continue;
+
+            int cu=__builtin_ctz(mask[u]);
+            color[u]=cu;
+            for(int v:g[u])
+            {
+                if(color[v]!=-1)
+                    continue;
+
+                if(mask[v]&(1<<cu))
+                {
+                    mask[v]^=(1<<cu);
+                    if(__builtin_popcount(mask[v])==1)
+                        stk.push_back(v);
+                }
+            }
+        }
+    };
+
+    for(int i=1;i<=n;i++)
+    {
+        if(color[i]==-1)
+        {
+            int c=__builtin_ctz(mask[i]);
+            assign_color(i,c);
+        }
+    }
+
+    string ans(n,'a');
+    for(int i=1;i<=n;i++)
+        ans[i-1]=char('a'+color[i]);
+    cout<<ans<<endl;
 }
 
 signed main()
@@ -23,10 +79,7 @@ signed main()
     cin.tie(nullptr), cout.tie(nullptr);
     //cout<<fixed<<setprecision(15);
 
-    int T;
-    cin>>T;
-    while(T--)
-        solve();
+    solve();
 
     return 0;
 }
