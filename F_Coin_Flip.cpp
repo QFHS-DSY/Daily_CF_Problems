e94bff16eb4ee509feebebdb38a65dd2e0c01307
杜松煜 <dsy20070704@163.com>
2026-06-30 14:47:13 +0800

    260630

--- F_Coin_Flip.cpp
+++ F_Coin_Flip.cpp
@@ -2,20 +2,15 @@
 //260630
 //1600
 #include <bits/stdc++.h>
-#define endl '\n'
 #define fi first
 #define se second
 #define PII pair<int, int>
 #define INF 0x3f3f3f3f
 #define LLINF 0x3f3f3f3f3f3f3f3fLL
-// #define int long long
+//#define int long long
 using namespace std;
 using ll=long long;
 using ld=long double;
-
-inline void solve()
-{
-}
 
 signed main()
 {
@@ -23,10 +18,36 @@ signed main()
     cin.tie(nullptr), cout.tie(nullptr);
     //cout<<fixed<<setprecision(15);
 
-    int T;
-    cin>>T;
-    while(T--)
-        solve();
+    int n,k,x;cin>>n>>k;
+    vector<PII> even,odd;
+    for (int i=1;i<=n;++i) {
+        cin>>x;
+        if (i&1) odd.push_back({x,i});
+        else even.push_back({x,i});
+    }
+    sort(odd.begin(),odd.end(),[](PII a,PII b){return a.fi>b.fi;});
+    sort(even.begin(),even.end(),[](PII a,PII b){return a.fi>b.fi;});
+    
+    int a=0,b=0;
+    while (k--) {
+        cin>>x;
+        if (x==1) {
+            //cerr<<odd[a].fi<<" "<<even[b+k].fi<<endl;
+            if (odd[a].fi>=even[b+k].fi) {
+                cout<<odd[a].se<<endl;
+                a++;
+            } else {
+                cout<<even[b].se<<endl;
+                b++;
+            }
+        } else {
+            cout<<even[b].se<<endl;
+            b++;
+        }
+
+        cin>>x;
+        if (x==0) return 0;
+    }
 
     return 0;
 }
