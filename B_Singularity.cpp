6c99f217b5f394ac65e8044282409f531dad85a6
杜松煜 <dsy20070704@163.com>
2026-07-01 15:31:15 +0800

    260701

--- B_Singularity.cpp
+++ B_Singularity.cpp
@@ -15,6 +15,35 @@ using ld=long double;
 
 inline void solve()
 {
+    int n,t,f=1;cin>>n;
+    vector<int> a(n+1);
+    for (int i=1;i<=n;++i) {
+        cin>>a[i];
+        if (i==1) t=a[i];
+        else if (t!=a[i]) f=0;
+    }
+
+    if (f) cout<<(a[1]==1 ? 0 : 1)<<endl;
+    else {
+        int l=1,r=n;
+        while (l<=n) {
+            if (a[l]==1) l++;
+            else break;
+        }
+        while (r>=1) {
+            if (a[r]==1) r--;
+            else break;
+        }
+
+        int flag=1;
+        for (int i=l;i<=r;++i) {
+            if (i==l) t=a[i];
+            else if (t!=a[i]) flag=0;
+        }
+
+        if (flag) cout<<1<<endl;
+        else cout<<2<<endl;
+    }
 }
 
 signed main()
