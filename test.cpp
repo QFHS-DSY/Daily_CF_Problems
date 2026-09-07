66b79c357897171dfc8b249eb61ff93fbff31205
杜松煜 <luoyegg@126.com>
2026-09-07 16:47:50 +0800

    Ignore binary files

--- test.cpp
+++ test.cpp
@@ -1,11 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int add(int x=5,int y=6) {
-    return x+y;
+int add(int x,int y=6,int z=10) {
+    return x+y+z;
 }
 
 int main () {
-    cout<<add(10)<<endl;
+    cout<<add(100,200)<<endl;
     return 0;
 }
